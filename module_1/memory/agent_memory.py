68d1cd9db0418c0bae4eecff4c422b4e48fc4f59
norberMV <norbermv.dev@gmail.com>
2025-07-13 16:56:51 -0500

    Passing memory through messages example.

--- module_1/memory/agent_memory.py
+++ module_1/memory/agent_memory.py
@@ -15,3 +15,25 @@ messages = [
 
 response = generate_responses(messages)
 print(response)
+
+messages.extend(
+    [
+        # Here is the assistant's response from the previous step
+        # with the code. This gives it "memory" of the previous
+        # interaction.
+        {
+            "role": "assistant", 
+            "content": response
+        },
+   
+        # Now, we can ask the assistant to update the function
+        {
+            "role": "user",
+            "content": "Update the function to include documentation in `google style`."
+        }
+    ]
+)
+print(f"Second messages iteration:\n{messages}")
+
+response = generate_responses(messages)
+print(response)
